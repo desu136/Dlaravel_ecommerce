ca4219098d0630e467b24c7d60dc90316ce717e2
desu136 <desukasaye19@gmail.com>
2025-12-05 03:43:02 +0900

    Enhance index.php with maintenance and request handling
    Refactor index.php for improved readability and structure.

--- api/index.php
+++ api/index.php
@@ -1,20 +1,32 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+// Maintenance mode check
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
+    exit;
 }
 
-// Register the Composer autoloader...
+// Composer autoload
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
+// Bootstrap the application
 $app = require __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+/** @var Kernel $kernel */
+$kernel = $app->make(Kernel::class);
+
+// Handle the request
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+// Send response to browser
+$response->send();
+
+// Terminate kernel (important for queues, sessions, etc.)
+$kernel->terminate($request, $response);
