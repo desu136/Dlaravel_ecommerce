1019428095931d8eff6157e5e8172c8d31f154c2
Desu136 <desukasaye19@gmail.com>
2025-12-05 03:46:24 +0900

    index versel modification for deployment

--- api/index.php
+++ api/index.php
@@ -18,3 +18,35 @@ require __DIR__.'/../vendor/autoload.php';
 $app = require __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
+<?php
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Maintenance mode check
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+    exit;
+}
+
+// Composer autoload
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap the application
+$app = require __DIR__.'/../bootstrap/app.php';
+
+/** @var Kernel $kernel */
+$kernel = $app->make(Kernel::class);
+
+// Handle the request
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+// Send response to browser
+$response->send();
+
+// Terminate kernel (important for queues, sessions, etc.)
+$kernel->terminate($request, $response);
