d6395d73121013e79f26dc76eb3c7d49dd14acf6
Desu136 <desukasaye19@gmail.com>
2025-12-05 21:15:12 +0900

    api index update

--- api/index.php
+++ api/index.php
@@ -1,55 +1,20 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Maintenance mode check
+// Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
-    exit;
 }
 
-// Composer autoload
+// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap the application
-$app = require __DIR__.'/../bootstrap/app.php';
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-<<<<<<< HEAD
 $app->handleRequest(Request::capture());
-<?php
-
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-// Maintenance mode check
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-    exit;
-}
-
-// Composer autoload
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap the application
-$app = require __DIR__.'/../bootstrap/app.php';
-
-=======
->>>>>>> 2ca45be2da60b8d5c4f04e081e4ca144ed64777a
-/** @var Kernel $kernel */
-$kernel = $app->make(Kernel::class);
-
-// Handle the request
-$response = $kernel->handle(
-    $request = Request::capture()
-);
-
-// Send response to browser
-$response->send();
-
-// Terminate kernel (important for queues, sessions, etc.)
-$kernel->terminate($request, $response);
